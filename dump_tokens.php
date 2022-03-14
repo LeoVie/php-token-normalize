10277d0bb6ce4b233296c4745a54939eb8767b76
Leo Viezens <leo.viezens@web.de>
2022-03-14 12:34:28 +0100

    Add LICENSE

--- dump_tokens.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$code = "<?php function foo(): void {} foo(); $bar = 'def';";
-
-$tokens = PhpToken::tokenize($code);
-
-foreach ($tokens as $token) {
-    print($token->id . ' -> ' . $token->getTokenName() . ': "' . $token->text . "\"\n");
-}
