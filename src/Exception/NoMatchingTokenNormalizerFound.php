1a32ffa2a4b9be2b1ac784e275085a21f04e878d
Leo Viezens <leo.viezens@web.de>
2022-05-21 20:37:13 +0200

    Upgrade Symfony to 6.0, fix deprecations (#3)

--- src/Exception/NoMatchingTokenNormalizerFound.php
+++ src/Exception/NoMatchingTokenNormalizerFound.php
@@ -5,17 +5,14 @@ declare(strict_types=1);
 namespace LeoVie\PhpTokenNormalize\Exception;
 
 use Exception;
-use Safe\Exceptions\StringsException;
 
 class NoMatchingTokenNormalizerFound extends Exception
 {
-    /** @throws StringsException */
     private function __construct(string $tokenType)
     {
-        parent::__construct(\Safe\sprintf('No matching token normalizer found for token type %s.', $tokenType));
+        parent::__construct(sprintf('No matching token normalizer found for token type %s.', $tokenType));
     }
 
-    /** @throws StringsException */
     public static function create(string $tokenType): self
     {
         return new self($tokenType);
