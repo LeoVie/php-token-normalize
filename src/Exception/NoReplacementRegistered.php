dc0106ed6ab0a40cc45ee54878c03d3925d60fbd
Leo Viezens <leo.viezens@web.de>
2022-05-21 20:33:47 +0200

    Upgrade Symfony to 6.0, fix deprecations

--- src/Exception/NoReplacementRegistered.php
+++ src/Exception/NoReplacementRegistered.php
@@ -4,18 +4,15 @@ declare(strict_types=1);
 
 namespace LeoVie\PhpTokenNormalize\Exception;
 
-use Exception;
-use Safe\Exceptions\StringsException;
+use Exception;;
 
 class NoReplacementRegistered extends Exception
 {
-    /** @throws StringsException */
     private function __construct(string $original)
     {
-        parent::__construct(\Safe\sprintf('No replacement registered for %s.', $original));
+        parent::__construct(sprintf('No replacement registered for %s.', $original));
     }
 
-    /** @throws StringsException */
     public static function create(string $original): self
     {
         return new self($original);
